a24f7f5509c86618d731b83781ba034482aae759
Arafathislam <islamarafath315@gmail.com>
2022-12-31 01:12:05 +0600

    done

--- GApi/api/urls.py
+++ GApi/api/urls.py
@@ -3,10 +3,12 @@ from .import views
 
 
 urlpatterns = [
-     path('studentapi/',views.StudentList.as_view()),
-     path('showall/',views.StudentCreate.as_view()),
-     path('find/<int:pk>/',views.StudentRetrieve.as_view()),
-     path('update/<int:pk>/',views.StudentUpdate.as_view()),
-      path('delete/<int:pk>/',views.StudentDestroy.as_view()),
+    #  path('studentapi/',views.StudentList.as_view()),
+    #  path('showall/',views.StudentCreate.as_view()),
+    #  path('find/<int:pk>/',views.StudentRetrieve.as_view()),
+    #  path('update/<int:pk>/',views.StudentUpdate.as_view()),
+    #   path('delete/<int:pk>/',views.StudentDestroy.as_view()),
+      path('studentapi/',views.StudentLC.as_view()),
+      path('studentapi/<int:pk>/',views.StudentFUD.as_view()),
     
 ]
--- GApi/api/views.py
+++ GApi/api/views.py
@@ -5,49 +5,74 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin
 # Create your views here.
 
-class StudentList(GenericAPIView,ListModelMixin):
+
+#..................... normal way show and create data .............
+
+# class StudentList(GenericAPIView,ListModelMixin):
+#     queryset=Student.objects.all()
+#     serializer_class=StudentSerializer
+
+#     def get(self,request,*args,**kwargs):
+#         return self.list(request,*args,**kwargs)
+
+
+
+# class StudentCreate(GenericAPIView,CreateModelMixin):
+#     queryset=Student.objects.all()
+#     serializer_class=StudentSerializer
+
+
+
+
+#....................  optimize way.show and create  ...................
+
+class StudentLC(GenericAPIView,ListModelMixin,CreateModelMixin):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
 
     def get(self,request,*args,**kwargs):
         return self.list(request,*args,**kwargs)
-
-
-
-class StudentCreate(GenericAPIView,CreateModelMixin):
-    queryset=Student.objects.all()
-    serializer_class=StudentSerializer
-
     def post(self,request,*args,**kwargs):
         return self.create(request,*args,**kwargs)
 
 
-class StudentRetrieve(GenericAPIView,RetrieveModelMixin):
+# ................ normal way find ,update and delete ...............
+
+# class StudentRetrieve(GenericAPIView,RetrieveModelMixin):
+#     queryset=Student.objects.all()
+#     serializer_class=StudentSerializer
+
+#     def get(self,request,*args,**kwargs):
+#         return self.retrieve(request,*args,**kwargs)
+
+
+# class StudentUpdate(GenericAPIView,UpdateModelMixin):
+#     queryset=Student.objects.all()
+#     serializer_class=StudentSerializer
+
+#     def put(self,request,*args,**kwargs):
+#         return self.update(request,*args,**kwargs)
+
+# class StudentDestroy(GenericAPIView,DestroyModelMixin):
+#     queryset=Student.objects.all()
+#     serializer_class=StudentSerializer
+
+#     def delete(self,request,*args,**kwargs):
+#         return self.destroy(request,*args,**kwargs)
+
+
+# ................ optimize  way find ,update and delete ...............
+
+
+class StudentFUD(GenericAPIView,RetrieveModelMixin,UpdateModelMixin,DestroyModelMixin):
     queryset=Student.objects.all()
     serializer_class=StudentSerializer
 
     def get(self,request,*args,**kwargs):
         return self.retrieve(request,*args,**kwargs)
 
-
-class StudentUpdate(GenericAPIView,UpdateModelMixin):
-    queryset=Student.objects.all()
-    serializer_class=StudentSerializer
-
     def put(self,request,*args,**kwargs):
         return self.update(request,*args,**kwargs)
-
-
-class StudentUpdate(GenericAPIView,UpdateModelMixin):
-    queryset=Student.objects.all()
-    serializer_class=StudentSerializer
-
-    def put(self,request,*args,**kwargs):
-        return self.update(request,*args,**kwargs)
-
-class StudentDestroy(GenericAPIView,DestroyModelMixin):
-    queryset=Student.objects.all()
-    serializer_class=StudentSerializer
 
     def delete(self,request,*args,**kwargs):
         return self.destroy(request,*args,**kwargs)
